5d071fd40d9f0c6ceeaa43e15bf91434eaa7b872
nourinalam <nourinalam26@gmail.com>
2022-05-28 23:57:54 +0700

    menambah warna pada data grid

--- ExerciseDataBinding/Form3.cs
+++ ExerciseDataBinding/Form3.cs
@@ -19,10 +19,35 @@ namespace ExerciseDataBinding
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'prodiTIDataSet1.Mahasiswa' table. You can move, or remove it, as needed.
+
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
             this.mahasiswaTableAdapter.Fill(this.prodiTIDataSet1.Mahasiswa);
 
-        }
+            //membuat tulisan header tabel bold (tebal)
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.ColumnHeadersDefaultCellStyle.Font, FontStyle.Bold);
+
+            //membuat background pada isi tabel
+            this.dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+            this.dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+            this.dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+            this.dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            this.dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
+
+            //mengubah posisi text pada header dan isi tabel Jenis Kelamin ke tengah
+            this.dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            //mengganti warna font isi tabel
+            this.dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Purple;
+            this.dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.DarkBlue;
+            this.dataGridView1.Columns[2].DefaultCellStyle.ForeColor = Color.CadetBlue;
+            this.dataGridView1.Columns[3].DefaultCellStyle.ForeColor = Color.Green;
+            this.dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.DarkRed;
+            this.dataGridView1.BackgroundColor = Color.Black;
+
+        
+    }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
